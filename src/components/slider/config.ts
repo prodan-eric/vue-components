25d496e68a0b9c11f83b4e25a51025eaa315bfb3
Prodan Eric <ericprodan1920s@gmail.com>
2022-10-28 12:37:47 +0300

    slider fic

--- src/components/slider/config.ts
+++ src/components/slider/config.ts
@@ -1,10 +1,12 @@
-export const pickerWidth = 20;         // picker length in pixels
+export const VAL_PX_RATIO = 5
+
+export const min = -10    //minimum slider value
+export const max = 100    //maximium slider value
+export const sliderWidth = (max-min)*VAL_PX_RATIO
+
+export const pickerWidth = 20         // picker length in pixels
 export const leftBarColor = 'black'    // left bar color
 export const rightBarColor = 'gray'    // right bar color
 export const pickerColor = 'lightblue' // picker color
 export const step = 1                  // progress step
-export const min = -10                   //minimum slider value
-export const max = 100                  //maximium slider value
-export const sliderWidth = (max-min)*5; //slider length in pixels
 
-export const VAL_PX_RATIO = 5
